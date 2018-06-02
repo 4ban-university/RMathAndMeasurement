feebcb5711016e5a8d39f2d3552f0ccd0c97a582
Dmitry Kryukov <remasik@gmail.com>
2018-06-02 12:21:22 -0400

    Added initial file

---
+++ src/init.R
@@ -0,0 +1,15 @@
+# main file to call all the functions
+# Author: Dmitry Kryukov
+
+source("stdlib.R")
+source("min.R")
+source("max.R")
+source("mode.R")
+#source("median.R")
+#source("mean.R")
+#source("deviation.R")
+
+array <- c(1,2,3,4,5,6,5,7,8,9)
+print(paste0("Min: ", min(array)))
+print(paste0("Max: ", max(array)))
+print(paste0("Mode: ", mode(array)))
