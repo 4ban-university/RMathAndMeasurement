792e8cefe303aea87eb7179d78fcea197762e8a9
Dmitry Kryukov <remasik@gmail.com>
2018-06-02 12:15:06 -0400

    Updates

--- src/min.R
+++ src/min.R
@@ -1,15 +1,12 @@
 # Function min return the min element in array
 # Author Dmitry Kryukov
 
-min <- function(array){
-  res <- 9999999999999999
+Min <- function(array){
+  m <- 9999999999999999
   for(elem in array){
-    if(elem < res){
-      res <- elem
+    if(elem < m){
+      m <- elem
     }
   }
-  return(res)
+  return(m)
 }
-
-array <- c(100,5,4,2,5,3,4,5,2,4,3,5,10,3,2,87,34,545,4,3,4,5,5,75,9,73,53,42,451,334,38,2,332,35,119)
-print(min(array))
