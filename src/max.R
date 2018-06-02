e6001313ee38bc3a39f8de6f4f0c779bc233807a
Dmitry Kryukov <remasik@gmail.com>
2018-06-02 12:15:41 -0400

    Updates

--- src/max.R
+++ src/max.R
@@ -1,15 +1,12 @@
 # Function return the maximum number in array
 # Author: Dmitry Kryukov
 
-max <- function(array){
-  res <- -9999999999999999
+Max <- function(array){
+  M <- -9999999999999999
   for(elem in array){
-    if(elem > res){
-      res <- elem
+    if(elem > M){
+      M <- elem
     }
   }
-  return(res)
+  return(M)
 }
-
-array <- c(1,5,4,2,5,3,4,5,2,4,3,5,4,3,2,87,34,545,4,3,4,5,5,75,9,73,53,42,451,334,38,2,332,35,119)
-print(max(array))
