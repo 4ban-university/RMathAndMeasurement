a3e980e1e1a55790a7029c57da2c6ce165f26285
Dmitry Kryukov <remasik@gmail.com>
2018-06-03 12:13:19 -0400

    Added tests for Min and Max functions

---
+++ src/tests.R
@@ -0,0 +1,11 @@
+# Tests
+# Author: Dmitry Kryukov
+
+source("stdlib.R")
+source("min.R")
+source("max.R")
+source("mode.R")
+
+library('testthat')
+
+test_dir('../tests')
---
+++ tests/testMax.R
@@ -0,0 +1,8 @@
+expect_that(MaxF(c(1,2,3,4,5,6)), equals(6))
+expect_that(MaxF(c(1,2,3,1,1,1)), equals(3))
+expect_that(MaxF(c(5,5,5,5,5,5,5,5,5)), equals(5))
+expect_that(MaxF(c(1,1)), equals(1))
+expect_that(MaxF(c(1)), equals(1))
+expect_that(MaxF(c(5,8,6,7,5,6,9,8,6,5,7,4,7,8,9,6)), equals(9))
+expect_that(MaxF(c(4,2,3,4,5,5)), equals(5))
+expect_that(MaxF(c(4,2,3,4,3,5)), equals(5))
