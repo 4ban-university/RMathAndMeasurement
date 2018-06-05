06583cc48f964e5da27552f7aea953343312bf53
Dmitry Kryukov <remasik@gmail.com>
2018-06-04 20:26:16 -0400

    Updates

--- tests/testMax.R
+++ tests/testMax.R
@@ -1,8 +1,15 @@
-expect_that(MaxF(c(1,2,3,4,5,6)), equals(6))
-expect_that(MaxF(c(1,2,3,1,1,1)), equals(3))
-expect_that(MaxF(c(5,5,5,5,5,5,5,5,5)), equals(5))
-expect_that(MaxF(c(1,1)), equals(1))
-expect_that(MaxF(c(1)), equals(1))
-expect_that(MaxF(c(5,8,6,7,5,6,9,8,6,5,7,4,7,8,9,6)), equals(9))
-expect_that(MaxF(c(4,2,3,4,5,5)), equals(5))
-expect_that(MaxF(c(4,2,3,4,3,5)), equals(5))
+context("SOEN6611 tests Max")
+
+test_that("test min", {
+  expect_that(MaxF(c(1,2,3,4,5,6)), equals(6))
+  expect_that(MaxF(c(1,2,3,1,1,1)), equals(3))
+  expect_that(MaxF(c(5,5,5,5,5,5,5,5,5)), equals(5))
+  expect_that(MaxF(c(1,1)), equals(1))
+  expect_that(MaxF(c(1)), equals(1))
+  expect_that(MaxF(c(5,8,6,7,5,6,9,8,6,5,7,4,7,8,9,6)), equals(9))
+  expect_that(MaxF(c(4,2,3,4,5,5)), equals(5))
+  expect_that(MaxF(c(4,2,3,4,3,5)), equals(5))
+})
+
+# отрицательный массив
+# отрицательный массив с одним элементов
--- tests/testMin.R
+++ tests/testMin.R
@@ -1,9 +1,13 @@
-expect_that(MinF(c(1,2,3,4,5,6)), equals(1))
-expect_that(MinF(c(1,2,3,1,1,1)), equals(1))
-expect_that(MinF(c(1,1)), equals(1))
-expect_that(MinF(c(1)), equals(1))
-expect_that(MinF(c(5,8,6,7,5,6,9,8,6,5,7,4,7,8,9,6)), equals(4))
-expect_that(MinF(c(4,2,3,4,5,1)), equals(1))
+context("SOEN6611 tests min")
 
+test_that("test min", {
+  expect_that(MinF(c(1,2,3,4,5,6)), equals(1))
+  expect_that(MinF(c(1,2,3,1,1,1)), equals(1))
+  expect_that(MinF(c(1,1)), equals(1))
+  expect_that(MinF(c(1)), equals(1))
+  expect_that(MinF(c(5,8,6,7,5,6,9,8,6,5,7,4,7,8,9,6)), equals(4))
+  expect_that(MinF(c(4,2,3,4,5,1)), equals(1))
+})
 
-
+# отрицательный массив
+# отрицательный массив с одним элементов
