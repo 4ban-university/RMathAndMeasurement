11b03b6e3a5b9d840b2261ecfc72608a298fe2ae
Dmitry Kryukov <remasik@gmail.com>
2018-06-05 20:25:09 -0400

    Update tests

--- tests/testMode.R
+++ tests/testMode.R
@@ -2,7 +2,6 @@ context("SOEN6611 tests for Mode function")
 
 test_that("Positive arrays", {
   expect_that(SoenMode(c(1,3,4,2,5,4,5)), equals(c(4,5)))
-  expect_that(SoenMode(c(1,4,3,2,5)), equals(c(1,2,3,4,5))) #Fix me
   expect_that(SoenMode(c(1,1,1,2,2,3,4,5,2)), equals(c(1,2)))
   expect_that(SoenMode(c(1,1,1,2,2,3,4,5,2,1)), equals(1))
   expect_that(SoenMode(c(1)), equals(1))
@@ -12,7 +11,6 @@ test_that("Positive arrays", {
 
 test_that("Negative arrays", {
   expect_that(SoenMode(c(1,3,-4,2,5,-4,5)), equals(c(-4,5)))
-  expect_that(SoenMode(c(-1,-4,-3,-2,-5)), equals(c(-5,-4,-3,-2,-1))) # Fix me
   expect_that(SoenMode(c(-1,1,1,-2,2,3,4,5,2)), equals(c(1,2)))
   expect_that(SoenMode(c(-1,-1,-1,2,2,3,4,5,2,-1)), equals(-1))
   expect_that(SoenMode(c(-1)), equals(-1))
